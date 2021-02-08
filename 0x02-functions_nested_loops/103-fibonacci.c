b7fc3c66d6d57fadb692c72bac8381b15cb98c12
Beniyam Legesse <lbeniyam@gmail.com>
2021-02-08 14:23:20 +0000

    sum of even fibanoci series

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+unsigned long x, y, z, sum;
+z = 0;
+x = 0;
+y = 1;
+sum = 0;
+while (z < 4000000)
+{
+z = x + y;
+x = y;
+y = z;
+if (z % 2 == 0)
+{
+sum += z;
+}
+}
+printf("%lu\n", sum)
+return (0);
+}
