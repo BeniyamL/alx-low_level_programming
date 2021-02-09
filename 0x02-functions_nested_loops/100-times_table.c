a2e9f4e44c3af78339a059b15780ae027898c46e
Beniyam Legesse <lbeniyam@gmail.com>
2021-02-09 11:14:10 +0000

    times table modification

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,9 +8,7 @@
  */
 void print_times_table(int n)
 {
-int i;
-int j;
-int p;
+int i, j, p;
 if (n > 0 && n < 15)
 {
 for (i = 0; i <= n; i++)
@@ -30,13 +28,13 @@ _putchar(' ');
 _putchar((p / 10) + '0');
 _putchar((p % 10) + '0');
 }
-else if (p >= 100 && j !=0)
+else if (p >= 100 && j != 0)
 {
 _putchar((p / 100) + '0');
 _putchar(((p / 10) % 10) + '0');
 _putchar((p % 10) + '0');
 }
-else 
+else
 {
 _putchar(p + '0');
 }
