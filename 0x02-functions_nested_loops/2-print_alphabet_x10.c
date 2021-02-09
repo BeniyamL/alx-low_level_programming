8436b3f2e77c831c777912c58c3f5256aa975998
Beniyam Legesse <lbeniyam@gmail.com>
2021-02-09 09:07:26 +0000

    modification of 2 print aplhabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,4 @@
-#include <stdio.h>
 #include "holberton.h"
-/**
- * print_alphabet_x10 - function declaration
- */
-void print_alphabet_x10(void);
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-print_alphabet_x10();
-return (0);
-}
-
 /**
  *print_alphabet_x10 -prints alphabet in lower case 10 tiems
  *
