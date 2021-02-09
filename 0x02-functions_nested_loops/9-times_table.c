f05ad7d93e8ca6e0edbc29baaff8caf81c16503d
Beniyam Legesse <lbeniyam@gmail.com>
2021-02-09 09:26:57 +0000

    times table modification

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,15 +13,19 @@ for (i = 0; i < 10; i++)
 for (j = 0; j < 10; j++)
 {
 p = i * j;
-if (p < 10)
+if (p >= 10)
+{
+_putchar((p / 10) + '0');
+_putchar((p % 10) + '0');
+}
+else if ((p < 10) && (j != 0))
 {
 _putchar(' ');
 _putchar(p + '0');
 }
-else if (p >= 10)
+else
 {
-_putchar((p / 10) + '0');
-_putchar((p % 10) + '0');
+_putchar(p + '0');
 }
 if (j != 9)
 {
