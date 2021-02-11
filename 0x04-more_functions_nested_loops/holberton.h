293d8975954657e4f2241b625bf0422993ea6dd3
Beniyam Legesse <lbeniyam@gmail.com>
2021-02-11 07:12:15 +0000

    checks whether a character is a digit

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -2,6 +2,8 @@
 /**
  * _putchar - writes the given character
  * _isupper - determine whether a given character is in upper case
+ * _isdigit - checks wheter a given character is a digit
 */
 int _putchar(char);
 int _isupper(int c);
+int _isdigit(int c);
