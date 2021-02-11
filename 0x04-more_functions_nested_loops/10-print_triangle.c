3ef128a525b95dc352c8658b7d68f63868a68046
Beniyam Legesse <lbeniyam@gmail.com>
2021-02-11 14:39:15 +0000

    triangle correction

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,9 +7,9 @@
 void print_triangle(int size)
 {
 int i, j, k;
-for (i = size; i >= 0; i--)
+for (i = size; i > 0; i--)
 {
-for (j = 0; j < i ; j++)
+for (j = 0; j < i - 1; j++)
 {
 _putchar(' ');
 }
