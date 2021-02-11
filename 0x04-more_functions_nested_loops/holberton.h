b926929da1cc35cf4e5d906b76919232e911deb3
Beniyam Legesse <lbeniyam@gmail.com>
2021-02-11 07:36:39 +0000

    multiplication of two numbers

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -2,8 +2,10 @@
 /**
  * _putchar - writes the given character
  * _isupper - determine whether a given character is in upper case
- * _isdigit - checks wheter a given character is a digit
+ * _isdigit - checks whether a given character is a digit
+ * mul-find the multiplication of two numbers
 */
 int _putchar(char);
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
