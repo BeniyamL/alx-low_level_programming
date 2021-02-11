a8fa622d682556d7637cb2d699cc3522e37e819d
Beniyam Legesse <lbeniyam@gmail.com>
2021-02-11 12:28:25 +0000

    FizzBuzz in cprogramming

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - print number 1-100 with Fizz and Buzz on condition
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int i;
+for (i = 1; i <= 100; i++)
+{
+if ((i % 3 == 0) && (i % 5 != 0))
+{
+printf("%s ", "Fizz");
+}
+else  if ((i % 5 == 0) && (i % 3 != 0))
+{
+printf("%s ", "Buzz");
+}
+else  if ((i % 5 == 0) && (i % 3 == 0))
+{
+printf("%s ", "FizzBuzz");
+}
+else
+{
+printf("%d ", i);
+}
+}
+printf("\n");
+return (0);
+}
