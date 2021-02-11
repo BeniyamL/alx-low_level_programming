03bfe5be19d1729b51462abcb591daf29bd27557
Beniyam Legesse <lbeniyam@gmail.com>
2021-02-11 13:59:48 +0000

    Fizz Buzz with condition

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "holberton.h"
 /**
  * main - print number 1-100 with Fizz and Buzz on condition
  *
@@ -9,17 +10,17 @@ int main(void)
 int i;
 for (i = 1; i <= 100; i++)
 {
-if ((i % 5 == 0) && (i % 3 == 0))
+if (i % 5 == 0 && i % 3 == 0)
 {
-printf("%s ", "FizzBuzz");
+printf("FizzBuzz ");
 }
 else if (i % 3 == 0)
 {
-printf("%s ", "Fizz");
+printf("Fizz ");
 }
 else if (i % 5 == 0)
 {
-printf("%s ", "Buzz");
+printf("Buzz ");
 }
 else
 {
