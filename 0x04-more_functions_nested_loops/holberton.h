8579f8d6ca01ac836a775ee78dd9d1ccbab04b71
Beniyam Legesse <lbeniyam@gmail.com>
2021-02-11 13:33:31 +0000

    print triangle

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -7,6 +7,10 @@
  * print_numbers -print numbers from 0-9
  * print_most_numbers - print numbers from 0-9 except 2 and 4
  * more_numbers - print numbers from 0-14 10 times
+ * print_line - print straight line of length n
+ * print_diagonal - print diagonal line of length n
+ * print_square - print square of length size
+ * print_triangle - print triangle of length size
  */
 int _putchar(char);
 int _isupper(int c);
@@ -15,3 +19,7 @@ int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
