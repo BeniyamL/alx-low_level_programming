cba238e30003c4de87c1419f9090ff46e553b82d
Beniyam Legesse <lbeniyam@gmail.com>
2021-02-15 09:22:06 +0000

    print even characters

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+/**
+ * puts2 - print even characters
+ * @str: character to be printed
+ */
+void puts2(char *str)
+{
+  int i, j;
+  i = 0;
+  while (str[i] != '\0')
+    {
+      i++;
+    }
+
+  for (j = 0; j < i; j++)
+    {
+      if (j % 2 == 0)
+	{
+	  _putchar(str[j]);
+	}
+    }
+  _putchar('\n');
+}
