f7666f86c95ec7faa6272d9777b781256f5edda0
Beniyam Legesse <lbeniyam@gmail.com>
2021-02-15 09:33:56 +0000

    print half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+/**
+ * puts_halt - print half of the string
+ * @str: the string to be halfed
+ */
+void puts_half(char *str)
+{
+  int i, j;
+  i = 0;
+  while (str[i] != '\0')
+    {
+      i++;
+    }
+
+  if (i % 2 == 0)
+    {
+      j = i / 2;
+    }
+  else
+    {
+      j = (i - 1) / 2;
+    }
+
+  for (; j < i; j++)
+    {
+      _putchar(str[j]);
+    }
+  _putchar('\n');
+}
