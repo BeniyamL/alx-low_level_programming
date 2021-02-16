e38191dcc579861b66697d132b418cf2136dae73
Beniyam Legesse <lbeniyam@gmail.com>
2021-02-16 07:02:21 +0000

    print string in reverse order

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,11 +7,12 @@ void print_rev(char *s)
 {
   int i;
   while (s[i] != '\0')
-    {
+  {
       i++;
-    }
+  }
   for (; i > 0; i--)
-    {
+  {
       _putchar(s[i - 1]);
-    }
+  }
+  _putchar('\n');
 }
