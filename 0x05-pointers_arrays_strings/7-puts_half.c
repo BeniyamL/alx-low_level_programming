59b93a7fa3fcc063479a84fe96bc90a226228979
Beniyam Legesse <lbeniyam@gmail.com>
2021-02-16 08:02:55 +0000

    print half of a string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,28 +1,27 @@
+#include "holberton.h"
 /**
  * puts_halt - print half of the string
  * @str: the string to be halfed
  */
 void puts_half(char *str)
 {
-  int i, j;
-  i = 0;
-  while (str[i] != '\0')
-    {
-      i++;
-    }
-
-  if (i % 2 == 0)
-    {
-      j = i / 2;
-    }
-  else
-    {
-      j = (i - 1) / 2;
-    }
-
-  for (; j < i; j++)
-    {
-      _putchar(str[j]);
-    }
-  _putchar('\n');
+int i, j;
+i = 0;
+while (str[i] != '\0')
+{
+i++;
+}
+if (i % 2 == 0)
+{
+j = i / 2;
+}
+else
+{
+j = (i - 1) / 2;
+}
+for (; j < i; j++)
+{
+_putchar(str[j]);
+}
+_putchar('\n');
 }
