1618e464c9ea464753f7f3afba19e3210a8a5f64
Beniyam Legesse <lbeniyam@gmail.com>
2021-02-16 09:04:59 +0000

    copy string from source to destination

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,16 +7,15 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-  int i, j;
-  i = 0;
-  while (src[i] != '\0')
-    {
-      i++;
-    }
-  for (j = 0; j <= i; j++)
-    {
-      dest[j] = src[j];
-    }
-
-  return (dest);
+int i, j;
+i = 0;
+while (src[i] != '\0')
+{
+i++;
+}
+for (j = 0; j <= i; j++)
+{
+dest[j] = src[j];
+}
+return (dest);
 }
