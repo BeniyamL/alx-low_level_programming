a301b665427bd157070948f539c80f280f55dec9
Beniyam Legesse <lbeniyam@gmail.com>
2021-02-16 10:48:51 +0000

    print only numbers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,33 +6,33 @@
  */
 int _atoi(char *s)
 {
-  int j, n, sign;
-  j = 0;
-  n = 0;
-  sign = 1;
-  while (s[j] != '\0')
-    {
-      if (s[j] == '-')
-	{
-	  sign = sign * -1;
-	}
-      if (s[j] >= '0' && s[j] <= '9')
-	{
-	  n = (n * 10) + (s[j] - '0');
-	}
-      if (n != 0 && !(s[j] >= '0' && s[j] <= '9'))
-	{
-	  break;
-	}
-      j++;
-    }
-
-  if (n == 0)
-    {
-      return (0);
-    }
-  else
-    {
-      return (sign * n);
-    }
+int j, n, sign;
+j = 0;
+n = 0;
+sign = 1;
+while (s[j] != '\0')
+{
+if (s[j] == '-')
+{
+sign = sign * -1;
+}
+if (s[j] >= '0' && s[j] <= '9')
+{
+n = (n * 10) + (s[j] - '0');
+}
+if (n != 0 && !(s[j] >= '0' && s[j] <= '9'))
+{
+break;
+}
+j++;
+}
+if (n == 0)
+{
+return (0);
+}
+else
+{
+n = (sign *n);
+return (n);
+}
 }
