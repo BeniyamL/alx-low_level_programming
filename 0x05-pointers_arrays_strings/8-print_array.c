981b8e6549ded8db57941a26641a18d99b34bfb2
Beniyam Legesse <lbeniyam@gmail.com>
2021-02-16 08:42:20 +0000

    nth element of an array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+# include <stdio.h>
 /**
  * print_array - print the n array
  * @a: array to be printed
@@ -7,7 +7,7 @@
 void print_array(int *a, int n)
 {
 int i;
-for (i = 0; i < n; i++)
+for (i = 0; i < n - 1; i++)
 {
 printf("%d, ", a[i]);
 }
