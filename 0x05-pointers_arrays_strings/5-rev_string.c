67f49b1e9331758c454a99cef69a611537e82317
Beniyam Legesse <lbeniyam@gmail.com>
2021-02-16 07:25:15 +0000

    revers string using pointer

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,22 +5,21 @@
  */
 void rev_string(char *s)
 {
-  int i, k, j;
-  k = 0;
-  while (s[k] != '\0')
-    {
-      k++;
-    }
-  char str[k];
-  j = 0;
-  for (i = k; i > 0; i--)
-    {
-      str[j] = s[i - 1];
-      j++;
-    }
-
-  for (i = 0; i < k; i++)
-    {
-      s[i] = str[i];
-    }
+int i, k, j;
+k = 0;
+while (s[k] != '\0')
+{
+k++;
+}
+char str[k];
+j = 0;
+for (i = k; i > 0; i--)
+{
+str[j] = s[i - 1];
+j++;
+}
+for (i = 0; i < k; i++)
+{
+s[i] = str[i];
+}
 }
