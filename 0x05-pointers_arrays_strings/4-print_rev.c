7fa3d31ec3ac4dfe783f2abe5acedb8e2566d2ae
Beniyam Legesse <lbeniyam@gmail.com>
2021-02-16 06:58:15 +0000

    string in revers postion

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,16 +1,17 @@
+#include "holberton.h"
 /**
  * print_rev - print the string in revers order
  * @s: string to be reversed
  */
 void print_rev(char *s)
 {
-int i;
-while (s[i] != '\0')
-{
-  i++;
-}
-for (; i > 0; i--)
-{
-_putchar(s[i - 1]);
-}
+  int i;
+  while (s[i] != '\0')
+    {
+      i++;
+    }
+  for (; i > 0; i--)
+    {
+      _putchar(s[i - 1]);
+    }
 }
