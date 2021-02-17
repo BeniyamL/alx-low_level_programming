1a603ab367308f08342172eafa59dd7ce6145717
Beniyam Legesse <lbeniyam@gmail.com>
2021-02-17 02:00:34 -0800

    convert string to integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,25 +7,27 @@
  */
 int _atoi(char *s)
 {
-int j, n, sign;
-j = 0;
-n = 0;
-sign = 1;
-while (s[j] != '\0')
-{
-if (s[j] >= '0' && s[j] <= '9')
-{
-n = (n * 10) + (s[j] - '0');
-}
-if (n != 0 && !(s[j] >= '0' && s[j] <= '9'))
-{
-break;
-}
-if (s[j] == '-')
-{
-sign = sign * -1;
-}
-j++;
-}
-return (sign *n);
+	int j, sign;
+	unsigned int n;
+
+	j = 0;
+	sign = 1;
+	n = 0;
+	while (s[j] != '\0')
+	{
+		if (s[j] >= '0' && s[j] <= '9')
+		{
+			n = (n * 10) + (s[j] - '0');
+		}
+		if (n != 0 && !(s[j] >= '0' && s[j] <= '9'))
+		{
+			break;
+		}
+		if (s[j] == '-')
+		{
+			sign = sign * -1;
+		}
+		j++;
+	}
+	return (sign * n);
 }
