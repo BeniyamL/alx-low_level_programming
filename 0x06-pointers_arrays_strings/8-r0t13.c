9282ec24a61f44882c0d07c0530a79b5b3effee8
Beniyam Legesse <lbeniyam@gmail.com>
2021-02-18 06:51:53 -0800

    encode string using rot 13

---
+++ 0x06-pointers_arrays_strings/8-r0t13.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * rot13 - function that encode string base on rotate 13 digit
+ * @s: the string to be encoded
+ *
+ * Return: the encoded string
+ */
+char *rot13(char *s)
+{
+	char ltr = {
+	"NOPQRSTUVWXYZABCDEFGHIJKLM[\\]^_`nopqrstuvwxyzabcdefghijklm"
+	};
+
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'A' && s[i] <= 'z')
+			s[i] = c[s[i] - 'A'];
+	}
+	return (s);
+}
