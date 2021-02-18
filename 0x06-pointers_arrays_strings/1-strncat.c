221332f34c442d2c1e9eb772f28e733bf60c60c2
Beniyam Legesse <lbeniyam@gmail.com>
2021-02-18 03:03:04 -0800

    concatenate n characters

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * _strncat - function to concatenate n character
+ * @src: the source string
+ * @dest: the destination string
+ * @n: the number of character
+ *
+ * Return: the concatenated string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] = '\0')
+	{
+		i++;
+	}
+	for (j = 0; j < n; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+	if (dest[i - 1] != '\0')
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}
