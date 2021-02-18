14789cb6b6246bc9a764acb02a7de47ec3bffb72
Beniyam Legesse <lbeniyam@gmail.com>
2021-02-18 07:22:47 -0800

    compare two string correction

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,21 +8,18 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int j, k;
+	int i;
 
-	k = 0;
-	j = 0;
-	while ((s1[j] != '\0') && (s2[j] != '\0'))
+	i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		if (s1[j] != s2[j])
-		{
-			k = 1;
-			return (s1[j] - s2[j]);
-		}
-		j++;
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
 	}
-	if (k == 0)
-	{
+
+	if (s1[i] == s2[i])
 		return (0);
-	}
+	return (s1[i] - s2[i]);
 }
