f8eeed4b5dbe243b9f3deca526187143244ebca5
Beniyam Legesse <lbeniyam@gmail.com>
2021-02-19 05:42:21 -0800

    print numbers using putchar

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -9,63 +9,62 @@
  */
 void ReduceTo100(int n, char sign)
 {
-  int temp, rmdr;
-  int firstDigit, secondDigit;
+	int temp, rmdr;
+	int firstDigit, secondDigit;
 
-  rmdr = n % 10;
-  n = n / 10;
+	rmdr = n % 10;
+	n = n / 10;
 
-  if (n < 100 && n >= 10)
-    {
-      firstDigit = n / 10;
-      secondDigit = n % 10;
-      _putchar(sign);
-      _putchar(firstDigit + '0');
-      _putchar(secondDigit + '0');
-    }
-  else if (n < 10)
-    {
-      _putchar(sign);
-      _putchar(n + '0');
-    }
-  else
-    {
-      ReduceTo100(n, sign);
-    }
-  _putchar(rmdr + '0');
+	if (n < 100 && n >= 10)
+	{
+		firstDigit = n / 10;
+		secondDigit = n % 10;
+		_putchar(sign);
+		_putchar(firstDigit + '0');
+		_putchar(secondDigit + '0');
+	}
+	else if (n < 10)
+	{
+		_putchar(sign);
+		_putchar(n + '0');
+	}
+	else
+	{
+		ReduceTo100(n, sign);
+	}
+	_putchar(rmdr + '0');
 }
 /**
  * print_number - function that print a number using putchar
  * @n: the number to be printed
- * 
+ *
  * Return: nothing
  */
-
 void print_number(int n)
 {
-  char sign = '\0';
-  int firstDigit, secondDigit;
+	char sign = '\0';
+	int firstDigit, secondDigit;
 
-  if (n < 0)
-    {
-      sign = '-';
-      n = n * -1;
-    }
-  if (n >= 100)
-    {
-      ReduceTo100(n, sign);
-    }
-  else if (n < 100 && n >= 10)
-    {
-      firstDigit = n / 10;
-      secondDigit = n % 10;
-      _putchar(sign);
-      _putchar(firstDigit + '0');
-      _putchar(secondDigit + '0');
-    }
-  else
-    {
-      _putchar(sign);
-      _putchar(n + '0');
-    }
+	if (n < 0)
+	{
+		sign = '-';
+		n = n * -1;
+	}
+	if (n >= 100)
+	{
+		ReduceTo100(n, sign);
+	}
+	else if (n < 100 && n >= 10)
+	{
+		firstDigit = n / 10;
+		secondDigit = n % 10;
+		_putchar(sign);
+		_putchar(firstDigit + '0');
+		_putchar(secondDigit + '0');
+	}
+	else
+	{
+		_putchar(sign);
+		_putchar(n + '0');
+	}
 }
