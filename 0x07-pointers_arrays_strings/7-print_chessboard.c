da3320c9879f402076e7a644bc3b25e7008aee2f
Beniyam Legesse <lbeniyam@gmail.com>
2021-02-23 03:34:20 -0800

    print chess board

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,16 +7,14 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	int x, y;
+	int i, j;
 
-	for (y = 0; y < 8; y++)
+	for (i = 0; i < 8; i++)
 	{
-		for (x = 0; x <= 8; x++)
+		for (j = 0; j < 8; j++)
 		{
-			if (x == 8)
-				_putchar('\n');
-			else
-				_putchar(a[y][x]);
+			_putchar(a[i][j]);
 		}
+		_putchar('\n');
 	}
 }	
