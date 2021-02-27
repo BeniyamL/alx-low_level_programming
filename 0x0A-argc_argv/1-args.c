855a0d8b57986128c86066cd1560538518a6c185
Beniyam Legesse <lbeniyam@gmail.com>
2021-02-27 03:24:20 -0800

    number of arguments to main function

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - the begninig of the execution
+ * @argc: the number of argument
+ * @argv: the argument it self
+ *
+ * Return: always success
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%d\n", (argc - 1));
+	return (0);
+}
