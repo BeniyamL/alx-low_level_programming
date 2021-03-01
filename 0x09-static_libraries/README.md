6a030addfcd3079350a5c77e0ff69b4a9ed52073
Beniyam Legesse <lbeniyam@gmail.com>
2021-03-01 00:54:01 -0800

    static library correction

--- 0x09-static_libraries/README.md
+++ 0x09-static_libraries/README.md
@@ -1,1 +1,1 @@
-This folder containes exercise for static libraries and argument of the main function i.e argc argv
+This folder containes exercise for static libraries 
--- 0x09-static_libraries/holberton.h
+++ 0x09-static_libraries/holberton.h
@@ -1,25 +1,6 @@
-/**
- * _putchar - function to print a string on screen
- * _islower - fucntion that converts string to lower case
- * _isalpha - function check whether a character is a string
- * _abs - function that finds the absolute value of a number
- * _isupper - functinon that converts string to uper case
- * _isdigit - function that checks wheter a character is a number
- * _strlen - function that finds the length of a string
- * _puts - fucntion that prints a string
- * *_strcpy - fucntion that copy a string 
- * _atoi - fucntion converts a string to number
- * *_strcat - function that concatenate a string
- * *_strncat - fucntion that concatenate n strings
- * *_strncpy - function that copies n characters
- * _strcmp - function that compares two string
- * *_memset - function that puts bytes of string
- * *_memcpy - function that copy a string
- * *_strchr - fucntion that takes a character
- * _strspn - function for string pointer
- * *_strpbrk - fucntion for string
- * *_strstr - function for finding a string
- */
+#ifndef holberton_h
+#define holberton_h
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -41,3 +22,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
+#endif
