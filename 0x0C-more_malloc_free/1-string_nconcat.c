b5f2043bb6dc57dcebb28cb4dcf8855d459108ab
Beniyam Legesse <lbeniyam@gmail.com>
2021-03-04 04:36:57 -0800

    string concatenation correction

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -36,7 +36,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		newarray[index] = s1[i];
 		index++;
 	}
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n && s2[i]; i++)
 	{
 		newarray[index] = s2[i];
 		index++;
