6915a7626c1a294d96bed5249047cc1967a5f167
root <lbeniyam@gmail.com>
2021-03-08 08:33:56 +0000

    create typedef from structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,11 +8,12 @@
  *
  * Desription: structure to define a dog
  */
-struct dog
+typedef struct dog
 {
 char *name;
 float age;
 char *owner;
-};
+}dog_t;
+void print_dog(struct dog *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
