b69930c445c9e4e0ccbecfe76f7f0602eef53f4e
Beniyam Legesse <lbeniyam@gmail.com>
2021-03-11 05:50:30 -0800

    correction of printing list of string

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -15,7 +15,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	char *s;
 
 	va_start(list, n);
-	if (separator == NULL || n < 0)
+	if (separator == NULL)
 		return;
 	for (i = 0; i < n; i++)
 	{
