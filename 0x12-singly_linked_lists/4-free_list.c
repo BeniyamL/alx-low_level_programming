126a30a4d0637d16a5583b2146dfacb46c717fe5
Beniyam Legesse <lbeniyam@gmail.com>
2021-03-18 04:10:40 -0700

    linked list exercise from 0- 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdarg.h>
+/**
+ * free_list - function that frees the linked list
+ * @head: the head of the linked list
+ *
+ * Return: return nothing
+ **/
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+		if (head->str != NULL)
+			free(head->str);
+		free(head);
+		head = temp;
+	}
+}
