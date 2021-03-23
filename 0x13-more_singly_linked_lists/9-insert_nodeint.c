50b3215af7e54a32fa2dbd54ed954b1c2287cc12
Beniyam Legesse <lbeniyam@gmail.com>
2021-03-23 01:37:41 -0700

    insert newnode at a given node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,8 +26,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	newnode->n = n;
 	if (idx == 0)
 	{
-		*head = newnode;
 		newnode->next = prevnode;
+		*head = newnode;
 		return (newnode);
 	}
 	while (counter < (idx - 1))
@@ -40,7 +40,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		prevnode = prevnode->next;
 		counter++;
 	}
-	prevnode->next = newnode;
 	newnode->next = prevnode->next;
+	prevnode->next = newnode;
 	return (newnode);
 }
