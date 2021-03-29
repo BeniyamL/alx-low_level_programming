cf12eb71eea482bae5d0e88fdf563aa693ad37a0
Beniyam Legesse <lbeniyam@gmail.com>
2021-03-29 07:25:53 -0700

    create a file correction

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 		i++;
 	fdw = write(fdo, text_content, i);
 	if (fdw == -1)
-		return (-1)
+		return (-1);
 	close(fdo);
 	return (1);
 }
