65e2b616316e74ca7e16b3f0f259b615d8fdf156
Beniyam Legesse <lbeniyam@gmail.com>
2021-05-28 11:11:18 -0700

    update of task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,7 +14,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	h_table = malloc(sizeof(hash_table_t));
 	if (h_table == NULL)
 		return (NULL);
-	h_node = malloc(size * sizeof(hash_node_t));
+	h_node = malloc(size * sizeof(hash_node_t *));
 	if (h_node == NULL)
 	{
 		free(h_table);
