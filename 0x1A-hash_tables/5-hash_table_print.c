8c3b5b88309b8081f5280fd3906421e1a3421b88
Beniyam Legesse <lbeniyam@gmail.com>
2021-05-28 11:44:49 -0700

    fix the format to print the hash table

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -21,7 +21,7 @@ void hash_table_print(const hash_table_t *ht)
 		{
 			if (flag == 1)
 				printf(", ");
-			printf("'%s' : '%s'", cur->key, cur->value);
+			printf("'%s': '%s'", cur->key, cur->value);
 			cur = cur->next;
 			flag = 1;
 		}
